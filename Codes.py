b8691cf6cc18fa7574f98b9522e09614cb5420ca
marceloferreir4 <marcelohenriquemhfp@gmail.com>
2020-10-08 13:41:17 -0300

    Update Codes.py

--- Codes.py
+++ Codes.py
@@ -1,6 +1,7 @@
 nome = input('Qual seu nome?')
 idade = input('Quantos anos você têm?')
 local = input('Onde mora?')
-print(f'Você de chama {nome}, mora em {local} e tem {idade} anos. Certo?')
+comida = input('Comida favorita')
+print(f'Você de chama {nome}, mora em {local} e tem {idade} anos e gosta de {comida}, certo?')
 
 
